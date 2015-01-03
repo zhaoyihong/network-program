488530360686bbcb9eea8c61131c81bd0a1c3c84
zhaoyihong <zhaoyihong@126.com>
2015-01-03 20:32:05 +0800

    使用readline解决tcp粘包问题

---
+++ tcp/03-nianbao-readline/echo_client.cpp
@@ -0,0 +1,70 @@
+/*************************************************************************
+	> File Name: echo_clien.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 2014年12月28日 星期日 14时08分59秒
+ ************************************************************************/
+#include<sys/types.h>      
+#include<iostream>
+#include<sys/socket.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<stdio.h>
+#include <string.h>
+#include <netinet/in.h>
+#include  <arpa/inet.h>
+#include "readline.h"
+
+using namespace std;
+
+void client_service(int);
+
+int main()
+{
+    int sock = socket(AF_INET,SOCK_STREAM,0);
+    if(sock < 0 )  err_exit("socket"); 
+    
+    struct sockaddr_in ser_addr;
+    memset(&ser_addr,0,sizeof(ser_addr));
+    ser_addr.sin_family = AF_INET;
+    ser_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    ser_addr.sin_port = htons(5188);
+    
+    int ret = connect(sock,(struct sockaddr*)&ser_addr,sizeof(ser_addr));
+    
+    if(ret == -1)
+    {
+        err_exit("conn");
+    }
+    
+    client_service(sock);
+
+    close(sock);
+
+    return 0;
+    
+}
+
+
+void client_service(int conn)
+{
+    char sendbuf[1024] = {0};
+    char recvbuf[1024] ={0};
+    int ret;
+
+    while(fgets(sendbuf,sizeof(sendbuf),stdin) != NULL)
+    {
+        writen(conn,sendbuf,strlen(sendbuf)); 
+
+        ret = readline(conn,recvbuf,1024);
+        if(0 == ret)
+        {
+            cout << "服务器关闭" << endl;
+            break;
+        }
+
+        printf("receiv:%s",recvbuf);
+        memset(&sendbuf,0,sizeof(sendbuf));
+        memset(&recvbuf,0,sizeof(recvbuf));
+    }
+}
---
+++ tcp/03-nianbao-readline/echo_ser.cpp
@@ -0,0 +1,124 @@
+/*************************************************************************
+	> File Name: echo_ser.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 2014年12月28日 星期日 13时18分27秒
+ ************************************************************************/
+#include<sys/types.h>      
+#include<iostream>
+#include<sys/socket.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<stdio.h>
+#include<string.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#include "readline.h"
+
+using namespace std;
+//void err_exit(const char *);
+void server_service(int);
+int main()
+{
+    int sock = socket(AF_INET,SOCK_STREAM,0);
+    if(sock < 0 )  err_exit("socket"); 
+    struct sockaddr_in ser_addr;
+    memset(&ser_addr,0,sizeof(ser_addr));
+    ser_addr.sin_family = AF_INET;
+    ser_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    ser_addr.sin_port = htons(5188);
+ 
+    int on = 1;
+    if(setsockopt(sock,SOL_SOCKET,SO_REUSEADDR,&on,sizeof(on)) < 0 )
+        err_exit("setsockopt");
+   
+    if(bind(sock,(sockaddr *)&ser_addr,sizeof(ser_addr)) == -1 )
+       err_exit("bind"); 
+
+    struct sockaddr_in local_addr;
+    socklen_t local_len = sizeof(local_addr);
+    if(getsockname(sock,(struct sockaddr *)&local_addr,&local_len) < 0)
+    {
+        err_exit("getsockname");
+    }
+    cout << "local addr:" << inet_ntoa(local_addr.sin_addr);
+    cout << " port:" << ntohs(local_addr.sin_port) << endl;
+
+
+    if(listen(sock,100) == -1)
+    {
+        err_exit("listen");
+    }
+   
+    int conn;
+
+    while(1)
+    {
+        struct sockaddr_in client_addr;
+        int len = sizeof(client_addr);
+        conn = accept(sock,(sockaddr *)&client_addr,(socklen_t*)&len);
+        if(conn == -1)
+        {
+            err_exit("conn");
+        }
+    
+        int fd = fork();
+        if(fd==-1)
+        {
+            err_exit("fork");
+        }
+
+        //父进程
+        if(fd  > 0)
+        {
+            close(conn);
+            continue;
+        }
+        else //子进程
+        {
+            close(sock);
+            server_service(conn);
+            close(conn);
+            break;
+        }
+    }
+    close(sock);
+    cout << "end" << endl;
+    return 0;
+}
+/*
+void err_exit(const char *msg)
+{
+    perror(msg);
+    exit(-1);
+}
+*/
+void server_service(int conn)
+{
+    struct sockaddr_in peeraddr;
+    socklen_t addrlen = sizeof(peeraddr);
+    if(getpeername(conn,(struct sockaddr *)&peeraddr,&addrlen) < 0)
+    {
+        err_exit("getpeername");
+    }
+    cout << "peer addr" << inet_ntoa(peeraddr.sin_addr) ;
+    cout << " port :" << ntohs(peeraddr.sin_port) << endl;
+
+
+    char buf[1024] = {0};
+
+    while(1)
+    {
+        int ret = readline(conn,buf,1024);
+        if(0 == ret)
+        {
+            cout << "客户端关闭" << endl;
+            break;
+        }
+
+        printf("receive:%s",buf);
+        writen(conn,buf,strlen(buf));
+        memset(buf,0,sizeof(buf));
+    }
+}
